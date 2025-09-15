7b636597078454701b49dd78107b4385b0d85888
gflores22 <gabrielchakalaka4@gmail.com>
2025-09-15 11:05:55 -0400

    Create Produto.java
    Criado a classe Produto que representa o produto no banco de dados e define regras de persistência.

---
+++ src/main/java/com/api/gerenciamento_produtos/model/Produto.java
@@ -0,0 +1,34 @@
+package com.api.gerenciamento_produtos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "produtos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 100)
+    private String descricao;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+}
