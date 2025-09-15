b2e886761cdd171f0ad47e8addb998a74b46cdc2
gflores22 <gabrielchakalaka4@gmail.com>
2025-09-15 10:49:35 -0400

    Primeiro commit

---
+++ src/main/java/com/api/gerenciamento_produtos/GerenciamentoProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.api.gerenciamento_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gerenciamento_produtos/GerenciamentoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gerenciamento_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
