3a182fe2058bb3e99035d8972a13a361bc18ef4b
gflores22 <gabrielchakalaka4@gmail.com>
2025-09-16 11:59:06 -0400

    feat: Implementado ProdutoService
    Criado o ProdutoService para regras de negócio e CRUD.
    
    fix: Alterado tipo de retorno do updateEntity no ProductMapper para Produto invés de void.

---
+++ src/main/java/com/api/gerenciamento_produtos/service/ProdutoService.java
@@ -0,0 +1,51 @@
+package com.api.gerenciamento_produtos.service;
+
+import com.api.gerenciamento_produtos.dto.ProdutoRequestDTO;
+import com.api.gerenciamento_produtos.mapper.ProdutoMapper;
+import com.api.gerenciamento_produtos.model.Produto;
+import com.api.gerenciamento_produtos.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository repository;
+    private final ProdutoMapper mapper;
+
+    public ProdutoService(ProdutoRepository repository, ProdutoMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public Produto salvarProduto(ProdutoRequestDTO requestDTO) {
+        Produto produto = mapper.toEntity(requestDTO);
+        return repository.save(produto);
+    }
+
+    public void deletarProduto(UUID id) {
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("Produto não encontrado com id " + id);
+        }
+        repository.deleteById(id);
+    }
+
+    public List<Produto> listarProdutos() {
+        return repository.findAll();
+    }
+
+    public Produto buscarProdutoPorId(UUID id) {
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Produto não encontrado com id " + id)
+        );
+    }
+
+    public Produto atualizarProduto(UUID id, ProdutoRequestDTO requestDTO) {
+        Produto produto = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Produto não encontrado com id " + id)
+        );
+        return mapper.updateEntity(produto, requestDTO);
+    }
+}
